f178b17ac144334a039897b9ab5a658dee0a336b
Aidan Murray <aidanhansm@gmail.com>
2020-09-23 09:42:32 -0600

    Add test for cookbook class

---
+++ test/cookbook_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+
+class CookbookTest < Minitest::Test
+  def test_exists_with_attributes
+    cookbook = CookBook.new
+
+    assert_instance_of CookBook, cookbook
+
+    assert_equal [] cookbook.recipes
+  end
+end
