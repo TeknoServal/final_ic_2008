2ee8e3d3750f27ffdb401154a5277f0783e4e38b
Aidan Murray <aidanhansm@gmail.com>
2020-09-23 09:11:30 -0600

    Add test file for ingredient class

---
+++ test/ingredient_test.rb
@@ -0,0 +1,13 @@
+require './test/test_helper'
+
+class IngredientTest < Minitest::Test
+  def test_exists_with_attributes
+    ingredient1 = Ingredient.new({ name: 'Cheese', unit: 'oz', calories: 50 })
+
+    assert_instance_of Ingredient, ingredient1
+
+    assert_equal 'Cheese', ingredient1.name
+    assert_equal 'oz', ingredient1.unit
+    assert_equal 50, ingredient1.calories
+  end
+end
