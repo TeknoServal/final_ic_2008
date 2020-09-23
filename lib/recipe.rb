f44290eb8324a35aca49fcbed4198d38258c2bf8
Aidan Murray <aidanhansm@gmail.com>
2020-09-23 09:30:54 -0600

    Add recipe class

---
+++ lib/recipe.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Recipe class
+class Recipe
+  attr_reader :name, :ingredients_required
+
+  def initialize(name)
+    @name = name
+    @ingredients_required = {}
+  end
+end
